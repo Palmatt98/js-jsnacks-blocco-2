8de6462c49782fa047bc0afa1fc4ad0ae3da981d
Palmatt98 <palmierimatteo98@gmail.com>
2024-04-05 12:37:26 +0200

    parola lunga parola corta

---
+++ parola lunga e corta/js/script.js
@@ -0,0 +1,10 @@
+// Esercizio 2
+// Dare la possibilità di inserire due parole.
+// Verificare tramite una funzione che le due parole abbiano la stessa lunghezza.
+// Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
+
+const word1 = prompt("inserisci la prima parola")
+const word2 = prompt("inserisci la seconda parola")
+
+const paroleStampate = inserisciParola();
+console.log(paroleStampate);
